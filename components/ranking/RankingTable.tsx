be748be726ed34b38c64105d795d843278c29460
jonaditommaso <jonaditommaso@gmail.com>
2023-02-09 00:41:25 -0300

    refactor: bootstrap table replaced with mui table

--- components/ranking/RankingTable.tsx
+++ components/ranking/RankingTable.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
-// import Review from '../../utils/Review';
-// import '../../styles/rankingTable.css';
 import { connect } from 'react-redux';
 import { RiErrorWarningLine } from 'react-icons/ri';
 import Review from '../../utils/Review';
 import styles from './ranking.module.css'
+import { Table, TableBody, TableCell, tableCellClasses, TableHead, TableRow, styled } from '@mui/material';
+import { capitalizeFirstLetter } from '../../utils/capitalizeFirstLetter';
+import { themePalette } from '../../config/theme.config';
 
 const RankingTable = ({ ranking }: any) => {
 
-    const [pokemonRanking, setPokemonRanking] = useState('');
+    const [pokemonRanking, setPokemonRanking] = useState([]);
 
     useEffect(() => {
         const getTeams = async() => {
@@ -23,29 +23,53 @@ const RankingTable = ({ ranking }: any) => {
         }
     }, [pokemonRanking, ranking]);
 
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: themePalette.blue,
+        color: theme.palette.common.white,
+        fontSize: 17,
+        padding: 10,
+        border: `1px solid ${themePalette.blue}`
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 15,
+        padding: 5,
+        borderLeft: '1px solid #e6e6e6',
+        borderRight: '1px solid #e6e6e6'
+    },
+    }));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: '#f7f7f7',
+  },
+}));
+
     return (
         <div className={styles.rankingTable}>
             { ranking.length === 0
             ? <div>No pokemons ranked <RiErrorWarningLine color="red" /> </div>
-            : <Table striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                        <th>Pokemon</th>
-                        <th>Ranking</th>
-                    </tr>
-                </thead>
-                {pokemonRanking ?
-                     <tbody >
-                    {pokemonRanking?.map((poke, i): any => (
-                        <tr key={i}>
-                            <td>{poke?.pokemon?.charAt(0).toUpperCase() + poke?.pokemon?.slice(1)}</td>
-                            <td><Review review={poke?.ranking} readOnly /></td>
-                        </tr>
-                    ))}
-                    </tbody>
-                    : null
-                }
-            </Table>
+            : <Table aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                {['Pokemon', 'Ranking'].map((th, index) => (
+                    <StyledTableCell key={`${th}-${index}`} component={'th'} align='center'>{th}</StyledTableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {pokemonRanking?.map((pokemon, index) => (
+                <StyledTableRow key={index} hover>
+                  <StyledTableCell align='center'>
+                    {capitalizeFirstLetter(pokemon?.pokemon)}
+                  </StyledTableCell>
+                  <StyledTableCell align='center'>
+                    <Review review={pokemon?.ranking} readOnly />
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
             }
         </div>
     );
