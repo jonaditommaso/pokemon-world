24ac43f937895308d074545e0ee3fa880d1d982b
jonaditommaso <jonaditommaso@gmail.com>
2023-04-27 20:23:07 -0300

    refactor: add more accepted symbols to regex

--- utils/validationSchemaSignIn.ts
+++ utils/validationSchemaSignIn.ts
@@ -10,7 +10,7 @@ export const validationSchemaSignIn = yup.object({
       .min(8, 'Password should be of minimum 8 characters length')
       .required('Password is required')
       .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-/_?=!@#\$%\^&\*])(?=.{8,})/,
         'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character'
       ),
 });
