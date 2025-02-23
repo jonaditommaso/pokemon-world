0a98ac78b94884ea6e1f50c4ec159aed8bd1c625
jonaditommaso <jonaditommaso@gmail.com>
2025-02-23 19:55:53 +0100

    chore: add click to component library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Head from 'next/head'
-import { Provider } from 'react-redux';
+import "../styles/globals.css";
+import { ClickToComponent } from "click-to-react-component";
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
+import { Provider } from "react-redux";
 
-import Navbar from '../components/navbar/Navbar'
-import { ThemeConfig } from '../config/theme.config';
-import { store } from '../redux/store';
+import Navbar from "../components/navbar/Navbar";
+import { ThemeConfig } from "../config/theme.config";
+import { store } from "../redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-
       <Head>
         <title>Pokemon World</title>
       </Head>
 
       <ThemeConfig>
         <Navbar />
+        <ClickToComponent />
         <Component {...pageProps} />
       </ThemeConfig>
-
     </Provider>
-  )
+  );
 }
